7021d5da85202c5ddc69848567c34a3c9d68f2a2
DianaApz <kari_dia_12@hotmail.com>
2018-09-03 04:43:25 -0500

    stats

---
+++ directory/dir1/md2.md
@@ -0,0 +1,6 @@
+- [Módulos(CommonJS)](https://nodejs.org/docs/latest-v0.10.x/api/modules.html)
+- [Semver](https://semver.org/)
+- [Path](https://nodejs.org/api/path.html)
+- [File System](https://nodejs.org/api/fs.html)
+- [marked](https://github.com/markedjs/marked)
+- [Asíncronía en js]
--- library.js
+++ library.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
-//get files()
 const [,, ...args] = process.argv;
-const path=require('path');
-const fs=require('fs')
-// let dir=process.cwd();
+const fetch=require('node-fetch');
 
+let options={
+    validate:false,
+    stats:false
+}
+
+const path=require('path');
+const fs = require('fs');
+const util = require('util');
+const Parser=require('markdown-parser');
+const parser= new Parser();
 
 const changeAbs=(insertPath)=>{
     if(path.isAbsolute(insertPath)){
@@ -28,23 +35,6 @@ const readDir = (dir,arrFile)=>{
    });
   return arrFile;
 };
-const fileOrDir=(insertPath)=>{
-    const change= changeAbs(insertPath);
-    fs.lstat(change, (err, stats) =>{
-        if (err) {
-            throw("Error");
-        } else if(stats.isDirectory()){
-            console.log('es carpeta')
-            // const answer=readDir(change);
-        } else if(stats.isFile()){
-           const ext=path.extname(change);
-            // if(ext==='.md'){
-                console.log('es archivo')
-            //   readFile(change);
-            // }
-        }
-   });
-}
 const readFile =(filemd,ar)=>new Promise((resolve,reject)=>{
     ar=ar || [];
     const read=util.promisify(fs.readFile);
@@ -64,6 +54,151 @@ const readFile =(filemd,ar)=>new Promise((resolve,reject)=>{
         })
     })
 });
+const answerStats=(arrObj)=>{
+    const arrlink=arrObj.map((obj)=>obj.href);
+    const uniques=arrlink.filter((el,index,array)=>array.indexOf(el) === index);
+    const obj={
+        total:arrlink.length,
+        unique:uniques.length
+    }
+    return obj;
+}
+const validateStatus=(arrlink)=>{
+    const arrPromise=arrlink.map(link=>{
+        return fetch(link);
+    })
+    
+    return Promise.all(arrPromise)
+    .then((res)=>{
+        const arrObjs=res.map((objRes)=>{
+            if(objRes.status === 200){
+                const obj={
+                    href:objRes.url,
+                    status:objRes.status,
+                    statusText:objRes.statusText
+                }
+                return obj
+            }else{
+                const obj={
+                    href:objRes.url,
+                    status:objRes.status,
+                    statusText:'FAIL'
+                }
+                return obj
+            }
+        })
+        return arrObjs
+    })
+}
+const answerValidate=(arrObj)=>{
+    let arrlink=arrObj.map((obj)=>obj.href);
+    let answer=validateStatus(arrlink);
+    return answer;
+}
+const answerBoth=(arrObj,validateLink)=>{
+    const arrlink=arrObj.map((obj)=>obj.href);
+    const uniques=arrlink.filter((el,index,array)=>array.indexOf(el) === index);
+    const obj={
+        total:arrlink.length,
+        unique:uniques.length,
+        broken:'falta'
+    }
+    return obj;
+}
+const mdlinks=(insertPath,options)=>new Promise((resolve,reject)=>{
+    const change= changeAbs(insertPath);
+    const stat=util.promisify(fs.lstat);
+    stat(change)
+    .then(stats=>{
+        if(stats.isDirectory()){
+            console.log('es car');
+            const answer=readDir(change);
+             answer.forEach(ele=>{
+                readFile(ele)
+                .then((arrObj)=>{
+                    if(options.validate===false&&options.stats===false){
+                        
+                        resolve(arrObj);
+                        
+                        
+                    }else if(options.validate===false&&options.stats===true){
+                        const answer=answerStats(arrObj);
+                        resolve(answer);
+                    }else if(options.validate===true&&options.stats===false){
+                        const answer=answerValidate(arrObj);
+                        setTimeout(()=>{
+                            resolve(answer);
+                        },5000);
+                
+                    }else if(options.validate===true&&options.stats===true){
+                        const answer=answerBoth(arrObj);
+                        resolve(answer);
+                    }
+                });
+            })
+        } else if(stats.isFile()){
+            const ext=path.extname(change);
+            if(ext==='.md'){
+                console.log('es file');
+                readFile(change)
+                .then((arrObj)=>{
+                    if(options.validate===false&&options.stats===false){
+                        resolve(arrObj);
+                    }else if(options.validate===false&&options.stats===true){
+                        const answer=answerStats(arrObj);
+                        resolve(answer);
+                    }else if(options.validate===true&&options.stats===false){
+                        const answer=answerValidate(arrObj);
+                        setTimeout(()=>{
+                            resolve(answer);
+                        },5000);
+                    }else if(options.validate===true&&options.stats===true){
+                        const answer=answerBoth(arrObj);
+                        resolve(answer);
+                    }
+                });
+            }
+        }
+    })
+    .catch((error) => {
+        console.log('errr');
+    });
+})
 
-let answer=fileOrDir(args[0]);
-// console.log(answer);
+if(args[0] !== '' && args[1] !== '--validate' && args[1] !== '--stats'){
+    mdlinks(args[0],options)
+    .then(res=>{
+        setTimeout(()=>{
+            console.log(res);
+        },5000);
+    })
+    .catch(error=>{
+        console.log('err')
+    });
+}
+if(args[1] === '--validate' && args[2] !== '--stats'){
+    options.validate=true;
+    mdlinks(args[0],options)
+    .then(res=>{
+        console.log(res)
+    }).catch(error=>{
+        console.log('err')
+    });
+}else if(args[1]==='--stats'&&args[2]!=='--validate'){
+    options.stats=true;
+    mdlinks(args[0],options)
+    .then(res=>{
+        console.log(res)
+    })
+    .catch(error=>{console.log('err')});
+}else if(args[1]==='--validate'&&args[2]==='--stats' || args[1]==='--stats'&&args[2]==='--validate'){
+    options.stats=true;
+    options.validate=true;
+    mdlinks(args[0],options)
+    .then(res=>{
+        console.log(res)
+    })
+    .catch(error=>{
+        console.log('err')
+    });
+}
